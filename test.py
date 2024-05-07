af0281607e02b1fbcbea754378cb84d8f1c06d7a
theadmaster <55393861+Theadmaster@users.noreply.github.com>
2024-05-07 21:12:32 +0800

    test update

--- test.py
+++ test.py
@@ -45,6 +45,6 @@ model = {
     'OneRel': models.RelModel
 }
 
-model_name = "OneRel_DATASET_ca_LR_1e-05_BS_4Max_len150Bert_ML300DP_0.2EDP_0.1"
+model_name = "OneRel_DATASET_ca_LR_5e-06_BS_4Max_len150Bert_ML300DP_0.2EDP_0.1"
 
 fw.testall(model[args.model_name], model_name)
